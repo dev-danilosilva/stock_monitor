f0c05272257c1014c6b1411ca79b1a9eb088cdee
dev-danilosilva <dev.danilosilva@gmail.com>
2020-02-16 00:54:32 -0300

    ADD create_stock method

--- stock_monitor/storage.py
+++ stock_monitor/storage.py
@@ -1,7 +1,7 @@
 from datetime import date
 from datetime import datetime
 from pony.orm import *
-from os import path
+import os
 
 db = Database()
 
@@ -17,14 +17,19 @@ class StockPrices(db.Entity):
     last_update = Required(datetime)
     close_price = Required(float)
     date = Required(date, unique=True)
-    stock__symbol = Required(StockSymbol)
+    fk_stock_id = Required(StockSymbol)
 
 
 class Storage:
-    def __init__(self, filename='database.sqlite'):
+    def __init__(self, filename='../database.sqlite'):
+        filename = os.path.join(os.getcwd(), filename)
         db.bind(provider='sqlite', filename=filename, create_db=True)
         db.generate_mapping(create_tables=True)
+
 
     def create_stock(self, tckr):
         with db_session:
             stock = StockSymbol(ticker=tckr)
+
+    def get_all_stocks(self):
+        pass
