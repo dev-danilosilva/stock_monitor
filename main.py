68b958ae3af01b7732b4b5696a27f9548c4ef145
dev-danilosilva <dev.danilosilva@gmail.com>
2020-02-17 00:36:52 -0300

    Fix bugs

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 from stock_monitor import AlphaVantageClient
 from stock_monitor.models import *
-from credentials import API_KEY
 from pony import *
 import datetime
+# from credentials import API_KEY
+
 
 # Para coletar dados de outras ações, adicione o símbolo na lista STOCK_SYMBOLS
 STOCK_SYMBOLS = ['PETR4.SAO', 'B3SA3.SAO']
 
+# Caminho do arquivo que armazenará os dados das ações listadas em STOCK_SYMBOLS
+DATABASE_FILE_PATH = 'database.db'
 
+#Chave para uso da AlphaVantageAPI
+API_KEY = ''
+
+
+# Caso uma determinada ação não exista no BD, um registro no banco de dados é criado. Caso já existe, o registro é
+# obtido do BD.
 def get_or_create_ticker(symbol):
     ticker = StockSymbol.get(ticker=symbol)
     if ticker is None:
@@ -17,13 +26,18 @@ def get_or_create_ticker(symbol):
 
 
 def main():
+    database_controller = connect_to_database(DATABASE_FILE_PATH)
     stock_api = AlphaVantageClient(API_KEY)
 
     with db_session:
         for ticker in STOCK_SYMBOLS:
 
             # Obtendo os dados dos ultimos 7 dias disponiveis na alphavantage api
-            api_data = stock_api.get_time_series_daily(ticker, period=7)
+            try:
+                api_data = stock_api.get_time_series_daily(ticker, period=7)
+            except Exception as e:
+                print('Error:', ticker)
+                continue
 
             # Obtendo os dados da acao do banco de dados
             db_stock = get_or_create_ticker(ticker)
@@ -42,6 +56,7 @@ def main():
                     )
                 elif stock_date_price != api_price:
                     stock_date_price.close_price = api_price
+            print('Success:', ticker)
 
 
 if __name__ == '__main__':
--- stock_monitor/models.py
+++ stock_monitor/models.py
@@ -3,16 +3,16 @@ from datetime import datetime
 from pony.orm import *
 import os
 
-database = Database()
+_database = Database()
 
 
-class StockSymbol(database.Entity):
+class StockSymbol(_database.Entity):
     id = PrimaryKey(int, auto=True)
     ticker = Required(str, unique=True)
     stock__prices = Set('StockPrices')
 
 
-class StockPrices(database.Entity):
+class StockPrices(_database.Entity):
     id = PrimaryKey(int, auto=True)
     last_update = Required(datetime)
     close_price = Required(float)
@@ -20,37 +20,10 @@ class StockPrices(database.Entity):
     fk_stock_id = Required(StockSymbol)
 
 
-filename = os.path.join(os.getcwd(), 'stock_monitor.db')
-database.bind(provider='sqlite', filename=filename, create_db=True)
-database.generate_mapping(create_tables=True)
-
-# class Storage:
-#     def __init__(self, filename='database.sqlite'):
-#         filename = os.path.join(os.getcwd(), filename)
-#         db.bind(provider='sqlite', filename=filename, create_db=True)
-#         db.generate_mapping(create_tables=True)
-#
-#     def create_stock(self, tckr):
-#         with db_session:
-#             stock = StockSymbol(ticker=tckr)
-#
-#     def define_price(self, stock: StockSymbol):
-#         with db_session:
-#             pass
-#
-#     def get_stock(self, ticker: str):
-#         with db_session:
-#             return StockSymbol.get(ticker=ticker)
-#
-#     def get_all_stocks(self):
-#         result = []
-#         with db_session:
-#             for res in select(stock for stock in StockSymbol):
-#                 result.append(res)
-#         return result
-#
-#     def save_changes(self, ticker):
-#         with db_session:
-#             commit()
+def connect_to_database(filename):
+    filename = os.path.join(os.getcwd(), filename)
+    _database.bind(provider='sqlite', filename=filename, create_db=True)
+    _database.generate_mapping(create_tables=True)
+    return _database
 
 
